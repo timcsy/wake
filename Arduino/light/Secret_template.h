9fe5a8cf9d94786f7a36c36311f4f604fb37996c
timcsy <timocsy@yahoo.com.tw>
2024-11-29 16:43:05 +0800

    update secret

--- Arduino/light/Secret_template.h
+++ Arduino/light/Secret_template.h
@@ -1,4 +1,4 @@
 #define WIFI_SSID "your-wifi-ssid"
 #define WIFI_PASSWORD "your-wifi-password"
 #define SERVER_IP "your-server-ip"
-#define SERVER_PORT "your-server-port"
+#define SERVER_PORT your-server-port
